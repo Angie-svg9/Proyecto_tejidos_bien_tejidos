e31a1de24ab3e21dec1e08d6803d4ea411cdc1e2
Angie-svg9 <25290879@utcalvillo.edu.mx>
2026-03-05 19:07:23 -0600

    Update lib.rs

--- lib.rs
+++ lib.rs
@@ -1,178 +1,156 @@
-use anchor_lang::prelude::*;
+Asi? use anchor_lang::prelude::*;
 declare_id!("BavuxHPkdt66ArWDT93jezxBqzvErwqw2DmfZZoJZY7B");
 
-#[program] 
-pub mod tienda_crochet {
-    use super::*;
+#[program] 
+pub mod tienda_crochet { // Tienda es como a se llama el archivo en el programa 
+    use super::*;
 
+    // Para crear la tienda y guardamos el nombre y el id del comprador
+    pub fn crear_tienda(context: Context<NuevaTienda>, nombre: String) -> Result<()> {
+       
+        let comprador_id = context.accounts.comprador.key();
+        msg!("Comprador_id: {}", comprador_id);
 
-    pub fn crear_tienda(context: Context<NuevaTienda>, nombre: String) -> Result<()> {
-       
-        let comprador_id = context.accounts.comprador.key();
-        msg!("Comprador_id: {}", comprador_id);
+        let hilos: Vec<Hilo> = Vec::new(); // Es la lista de Hilo vacía 
 
-        let hilos: Vec<Hilo> = Vec::new(); 
+        
+        context.accounts.tienda.set_inner(Tienda { 
+            comprador: comprador_id,
+            nombre,
+            hilos,
+            
+        });
+        Ok(()) 
+    }
 
-        
-        context.accounts.tienda.set_inner(Tienda { 
-            comprador: comprador_id,
-            nombre,
-            hilos,
-            
-        });
-        Ok(()) 
-    }
+   // Agregar un nuevo hilo a la lista y solo lo puede hacer el comprador
+    pub fn agregar_hilo(context: Context<NuevoHilo>, nombre: String, color: String, grosor: u16) -> Result<()> {
+        require!( 
+            context.accounts.tienda.comprador == context.accounts.comprador.key(), 
+            Errores::NoEresElComprador 
+        ); 
 
+        let nuevo_hilo = Hilo { 
+            nombre,
+            color,
+            grosor,
+            disponible: true,
+        };
 
-    pub fn agregar_hilo(context: Context<NuevoHilo>, nombre: String, color: String, grosor: u16) -> Result<()> {
-        require!( 
-            context.accounts.tienda.comprador == context.accounts.comprador.key(), 
-            Errores::NoEresElComprador 
-        ); 
+        context.accounts.tienda.hilos.push(nuevo_hilo); 
 
-        let nuevo_hilo = Hilo { 
-            nombre,
-            color,
-            grosor,
-            disponible: true,
-        };
+        Ok(()) 
+    }
 
-        context.accounts.tienda.hilos.push(nuevo_hilo); 
+    // Eliminar un hilo de la lista 
+    pub fn eliminar_hilo(context: Context<NuevoHilo>, nombre: String) -> Result<()> {
+        require!(
+            context.accounts.tienda.comprador == context.accounts.comprador.key(),
+            Errores::NoEresElComprador
+        );
+        // Buscar en la lista el hilo para saber que si sí está 
+        let hilos = &mut context.accounts.tienda.hilos;
+        for i in 0..hilos.len() {
+            if hilos[i].nombre == nombre { 
+                hilos.remove(i); // Para quitar de la lista 
+                msg!("Hilo {} eliminado!", nombre); // Para que mande un mensaje
+                return Ok(()); 
+            }
+        }
+        Err(Errores::HiloNoExiste.into()) // Por si el hilo no está mande un mensaje de error
+    }
 
-        Ok(()) 
-    }
+   // Para ver la lista de los hilos 
+    pub fn ver_hilos(context: Context<NuevoHilo>) -> Result<()> {
+        require!( 
+            context.accounts.tienda.comprador == context.accounts.comprador.key(),
+            Errores::NoEresElComprador // Error para cuando no es el comprador
+        );
 
-    
-    pub fn eliminar_hilo(context: Context<NuevoHilo>, nombre: String) -> Result<()> {
-        require!(
-            context.accounts.tienda.comprador == context.accounts.comprador.key(),
-            Errores::NoEresElComprador
-        );
+       
+        msg!("La lista de hilos actualmente es: {:#?}", context.accounts.tienda.hilos);
+        Ok(()) 
+    }
 
-        let hilos = &mut context.accounts.tienda.hilos;
-        for i in 0..hilos.len() { // Se itera mediante el indice todo el contenido del vector en busca del libro a eliminar
-            if hilos[i].nombre == nombre { // Si lo encuentra prodece a borrarlo mediante el metodo remove
-                hilos.remove(i);
-                msg!("Hilo {} eliminado!", nombre); // Mensaje de borrado exitoso
-                return Ok(()); // Transaccion exitosa
-            }
-        }
-        Err(Errores::HiloNoExiste.into()) // Transaccion fallida, nunca encontro el libro
-    }
+    
+    // Para saber si el hilo está disponible o no
+    pub fn alternar_hilo(context: Context<NuevoHilo>, nombre: String) -> Result<()> {
+        require!( 
+            context.accounts.tienda.comprador == context.accounts.comprador.key(),
+            Errores::NoEresElComprador // Si no es el comprador marca error
+        );
 
-   
-    pub fn ver_hilos(context: Context<NuevoHilo>) -> Result<()> {
-        require!( // Medida de seguridad 
-            context.accounts.tienda.comprador == context.accounts.comprador.key(),
-            Errores::HiloNoExiste
-        );
+        let hilos = &mut context.accounts.tienda.hilos; 
+        for i in 0..hilos.len() { 
+            let estado = hilos[i].disponible;
+            if hilos[i].nombre == nombre {
+                let nuevo_estado = !estado;
+                hilos[i].disponible = nuevo_estado;
+                msg!("El hilo: {} ahora esta disponible: {}", nombre, nuevo_estado);
+                return Ok(()); // Transaccion exitosa
+            }
+        }
 
-        // :#? requiere que NuevoLibro tenga atributo Debug. Permite la visualizacion completa del vector en el log
-        msg!("La lista de hilos actualmente es: {:#?}", context.accounts.tienda.hilos); // Print en log
-        Ok(()) // Transaccion exitosa 
-    }
-
-    
-    //////////////////////////// Instruccion: Alternar Estado /////////////////////////////////////
-    /* 
-    Cambia el estado de disponible de false a true o de true a false.
-
-    Parametros de entrada:
-        * nombre -> Nombre del libro -> string
-     */
-    pub fn alternar_hilo(context: Context<NuevoHilo>, nombre: String) -> Result<()> {
-        require!( // Medida de seguridad
-            context.accounts.tienda.comprador == context.accounts.comprador.key(),
-            Errores::NoEresElComprador
-        );
-
-        let hilos = &mut context.accounts.tienda.hilos; // Referencia mutable al vector de libros
-        for i in 0..hilos.len() { // Se itera mediante el indice el vector de libros
-            let estado = hilos[i].disponible;  // Se almacena el estado del vector actual
-
-            if hilos[i].nombre == nombre { // Si ecuentra el nombre del libro procede a cambiar el valor del estado 
-                let nuevo_estado = !estado;
-                hilos[i].disponible = nuevo_estado;
-                msg!("El hilo: {} ahora esta disponible: {}", nombre, nuevo_estado); // log print de la nueva disponibilidad
-                return Ok(()); // Transaccion exitosa
-            }
-        }
-
-        Err(Errores::HiloNoExiste.into()) // Transaccion fallida, libro no existe
-    }
+        Err(Errores::HiloNoExiste.into()) // Por si el hilo no existe mande error
+    }
 
 }
 
-/*
-Codigos de error
-Todos los codigos se almacenan en un enum con la siguiente estructura:
-#[msg("MENSAJE DE ERROR")] (dentro de las comillas)
-NombreDelError, (En camel case)
-*/
+
 #[error_code]
 pub enum Errores {
-    #[msg("Error, no eres el comprador del hilo que deseas modificar")]
-    NoEresElComprador,
-    #[msg("Error, el hilo con el que deseas interactuar no existe")]
-    HiloNoExiste,
+    #[msg("Error, no eres el comprador del hilo que deseas modificar")]
+    NoEresElComprador,
+    #[msg("Error, el hilo con el que deseas interactuar no existe")]
+    HiloNoExiste,
 }
 
-#[account] // Especifica que el strcut es una cuenta que se almacenara en la blockchain
-#[derive(InitSpace)] // Genera la constante INIT_SPACE y determina el espacio de almacenamiento necesario 
-pub struct Tienda { // Define la Biblioteca
-    pub comprador: Pubkey, // Pubkey es un formato de llave publica de 32 bytes 
+#[account] 
+#[derive(InitSpace)]
+pub struct Tienda { 
+    pub comprador: Pubkey, 
 
-    #[max_len(60)] // Cantidad maxima de caracteres del string: nombre
-   pub  nombre: String,
+    #[max_len(60)] 
+   pub  nombre: String,
 
-    #[max_len(10)] // Tamaño maximo del vector libros 
-   pub  hilos: Vec<Hilo>,
+    #[max_len(10)] 
+   pub  hilos: Vec<Hilo>,
 }
 
-/*
-Struct interno o secundario (No es una cuenta). Se define por derive y cuenta con los siguientes atributos:
-    * AnchorSerialize -> Permite guardar el struct en la cuenta 
-    * AnchorDeserialize -> Permite leer su contenido desde la cuenta 
-    * Clone -> Para copiar su contenido o valores 
-    * InitSpace -> Calcula el tamaño necesario para ser almacenado en la blockchain
-    * PartialEq -> Para usar sus valores y compararlos con "=="
-    * Debug -> Para mostrarlo en log con ":?" o ":#?"
-*/
+
 #[derive(AnchorSerialize, AnchorDeserialize, Clone, InitSpace, PartialEq, Debug)]
 pub struct Hilo {
-   #[max_len(60)]
-    pub nombre: String,
-    #[max_len(30)] 
-    pub color: String, 
-    pub grosor: u16, 
-    pub disponible: bool,
+   #[max_len(60)]  // Cantidad maxima de caracteres para nombre 
+    pub nombre: String,
+    #[max_len(30)]  // Cantidad maxima de caracteres para color
+    pub color: String, 
+    pub grosor: u16, 
+    pub disponible: bool,
 }
 
 
-// Creacion de los contextos para las instrucciones (funciones)
-#[derive(Accounts)] // Especifica que este struct describe las cuentas que se requieren para determinada instruccion
-pub struct NuevaTienda<'info> { // contexto de la instruccion
-    #[account(mut)] 
-    pub comprador: Signer<'info>, // Se define que el owner como el que pagara la transaccion, por eso es mut, para que cambie el balance de la cuenta
 
-    #[account(
-        init, // Inidica que al llamar la instruccuion se creara una cuenta
-        // puede ser remplazado por "init_if_needed" para que solo se cree una vez por caller
-        payer = comprador, // Se especifica que quien paga el llamado a la instruccion, en este caso llama la instruccion 
-        space = Tienda::INIT_SPACE + 8, // Se calcula el espacio requerido para almacenar el Solana Program On-Chain
-        seeds = [b"tienda", comprador.key().as_ref()], // Se especifica que la cuenta es una PDA que depende de un string y el id del owner
-        bump // Metodo para determinar el el id de la biblioteca en base a lo anterior 
-    )]
-    pub tienda: Account<'info, Tienda>, // Se especifica que la cuenta creada (PDA) almacenara la biblioteca 
+#[derive(Accounts)]
+pub struct NuevaTienda<'info> {
+    #[account(mut)] 
+    pub comprador: Signer<'info>,
 
-    pub system_program: Program<'info, System>, // Programa necesario para crear la cuenta 
+    #[account(
+        init, // Inidica que al llamar la instruccuion se creara una cuenta
+        // puede ser remplazado por "init_if_needed" para que solo se cree una vez por caller
+        payer = comprador, // Se especifica que quien paga el llamado a la instruccion, en este caso llama la instruccion 
+        space = Tienda::INIT_SPACE + 8, // Se calcula el espacio requerido para almacenar el Solana Program On-Chain
+        seeds = [b"tienda", comprador.key().as_ref()], // Se especifica que la cuenta es una PDA que depende de un string y el id del comprador
+        bump // Metodo para determinar el el id de la tienda  en base a lo anterior 
+    )]
+    pub tienda: Account<'info, Tienda>, 
+
+    pub system_program: Program<'info, System>, 
 }
-
-// Contexto para la creacion y modificacion de libros 
-#[derive(Accounts)] // Especifica que este struct se requiere para todas las instrucciones relacionadas con la creacion o modificacion de Libro
+#[derive(Accounts)]
 pub struct NuevoHilo<'info> {
-    pub comprador: Signer<'info>, // El owner de la cuenta es quien paga la transaccion
+    pub comprador: Signer<'info>,
 
-    #[account(mut)] 
-    pub tienda: Account<'info, Tienda>, // Se marca biblioteca como mutable porque se modificara tanto el vector como los libros que contiene
+    #[account(mut)] 
+    pub tienda: Account<'info, Tienda>, 
 }
